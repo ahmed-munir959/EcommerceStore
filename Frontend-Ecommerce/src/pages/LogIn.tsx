d25705ebba8ac3ac8d90cc48a05168e02d282ab2
Ahmed Munir <ahmed.munir.0660@gmail.com>
2026-02-12 21:52:37 +0500

    Update padding in LogIn component and adjust SVG class in SignUpUI for consistency

--- Frontend-Ecommerce/src/pages/LogIn.tsx
+++ Frontend-Ecommerce/src/pages/LogIn.tsx
@@ -2,7 +2,7 @@ import LoginUI from "../components/LogInUI";
 
 const LogIn = () => {
   return (
-    <main className="flex items-center justify-center pt-8 md:pt-12 lg:pt-[4rem] pb-12 md:pb-16 lg:pb-[8rem]">
+    <main className="flex items-center justify-center pt-8 md:pt-12 lg:pt-16 pb-12 md:pb-16 lg:pb-32">
       <LoginUI />
     </main>
   );
